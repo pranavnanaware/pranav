e339851e18fedccc5c5bb2e9b96572bb78c62b58
p-nanaware <pnanaware.umassd.edu>
2024-03-06 02:04:55 -0500

    Updated build command and headline

--- app/page.tsx
+++ app/page.tsx
@@ -31,16 +31,15 @@ export default function Home() {
           className="absolute inset-0 -z-10 animate-fade-in"
           quantity={100}
         />
-        <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        <h1 className="z-10 text-6xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
           pranav
         </h1>
 
         <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
         <div className="my-16 text-center animate-fade-in">
           <h2 className="text-sm text-zinc-500 ">
-            Join me, as I dissect the intricacies of software engineering,
-            cloud, devops and mobile apps, and the stories behind the projects
-            that have shaped my path in the tech realm.
+            Documenting my tech journey: Insights into engineering, AI, cloud
+            and devops.
           </h2>
         </div>
       </div>
