9f830529d8eedb2674bca4854a1f0ea53c9a4259
p-nanaware <pnanaware.umassd.edu>
2024-07-26 16:22:09 -0400

    Portfolio

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigation } from "../components/nav";
+
+const Portfolio = () => {
+  return (
+    <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+      <Navigation />
+    </div>
+  );
+};
+
+export default Portfolio;
